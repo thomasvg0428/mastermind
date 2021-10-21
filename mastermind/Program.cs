4ce134e72e9095af4ede40522077d51d180a7be0
thomasvg0428 <92924142+thomasvg0428@users.noreply.github.com>
2021-10-21 13:25:04 +0200

    projet mastermind

---
+++ mastermind/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] combi;
+            ChoixCombi(out combi);
+            Afficher(combi);
+        }
+
+        
+        static void ChoixCombi(out int[] combi){
+            combi = new int[4];
+            Random rnd = new Random();
+            for(int i = 0; i <= 3; i++){
+                int valeur  = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+
+        static void Afficher(int[] combi) {
+            for(int i = 0; i <= 3; i++){
+                Console.Write(combi[i]);
+            }
+        }
+		
+    }
+}
